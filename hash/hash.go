8c300adb6f0fab3aef5f6b0dcbcdc20903ffa7c7
Son Tran <286.trants@gmail.com>
2024-06-12 08:41:58 +0700

    Implement `hash` package - Package hash provides some classic hash functions(uint32/uint64) in go.

---
+++ hash/hash.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2022-2024 The Focela Authors, All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package hash provides some classic hash functions(uint32/uint64) in go.
+package hash
